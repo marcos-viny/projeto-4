8b4a57964f57de96a2946d773e0872fefce8eb05
Marcos Vinicius Silva <marcosviniciusmoreira00@gmail.com>
2021-12-15 16:52:43 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <h1 className="teste_1 text-3xl bg-red-400 font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
